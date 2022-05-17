bdc4352b83f390b4939aea2f78a229a6ef00b6e7
vetallb <garuk1vitalik@gmail.com>
2022-05-17 16:57:46 +0300

    m

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:app
+web: python http.py runserver 0.0.0.0:5000
--- http.py
+++ http.py
@@ -1,51 +1,51 @@
-# from http.server import BaseHTTPRequestHandler
-# from http.server import HTTPServer
-# import urllib
-# from loader import pgbotdb
-# class HttpGetHandler(BaseHTTPRequestHandler):
-#     """Обработчик с реализованным методом do_GET."""
+from http.server import BaseHTTPRequestHandler
+from http.server import HTTPServer
+import urllib
+from loader import pgbotdb
+class HttpGetHandler(BaseHTTPRequestHandler):
+    """Обработчик с реализованным методом do_GET."""
 
-#     def do_GET(self):
-#         self.send_response(200)
-#         self.send_header("Content-type", "text/html")
-#         self.end_headers()
-#         self.wfile.write('<html><head><meta charset="utf-8">'.encode())
-#         self.wfile.write('<title>CHAT</title></head>'.encode())
-#         self.wfile.write('<body>'.encode())
-#         self.comentmas=[]
-#         readData(self.comentmas)
-#         for el in self.comentmas:
-#             self.wfile.write(f'<h3>{el}</h3>'.encode())
-#         self.wfile.write('<form action="/page.py">'.encode())
-#         self.wfile.write('<input type="text" id="coment" name="coment">'.encode())
-#         self.wfile.write('<input type="submit">'.encode())
-#         self.wfile.write('</form>'.encode())
-#         self.wfile.write('</body></html>'.encode())
-#         req= urllib.parse.unquote(self.requestline)
-#         print(self.requestline)
-#         req = req.replace('GET /page.py?coment=','').replace(' HTTP/1.1', '')
-#         if req == '': return
-#         name_text = req.split(':')
-#         name = name_text[0].replace('+',' ')
-#         text = name_text[-1].replace('+',' ')
-#         print(name, text)
-#         pgbotdb.add_text(name, text,4)
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write('<html><head><meta charset="utf-8">'.encode())
+        self.wfile.write('<title>CHAT</title></head>'.encode())
+        self.wfile.write('<body>'.encode())
+        self.comentmas=[]
+        readData(self.comentmas)
+        for el in self.comentmas:
+            self.wfile.write(f'<h3>{el}</h3>'.encode())
+        self.wfile.write('<form action="/page.py">'.encode())
+        self.wfile.write('<input type="text" id="coment" name="coment">'.encode())
+        self.wfile.write('<input type="submit">'.encode())
+        self.wfile.write('</form>'.encode())
+        self.wfile.write('</body></html>'.encode())
+        req= urllib.parse.unquote(self.requestline)
+        print(self.requestline)
+        req = req.replace('GET /page.py?coment=','').replace(' HTTP/1.1', '')
+        if req == '': return
+        name_text = req.split(':')
+        name = name_text[0].replace('+',' ')
+        text = name_text[-1].replace('+',' ')
+        print(name, text)
+        pgbotdb.add_text(name, text,4)
 
-# # %3A
+# %3A
 
-# def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
-#     server_address = ("127.0.0.1", 8000)
-#     httpd = server_class(server_address, handler_class)
+def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
+    server_address = ("0.0.0.0", 5000)
+    httpd = server_class(server_address, handler_class)
 
-#     httpd.serve_forever()
+    httpd.serve_forever()
 
-# def readData(comentmas: list):
-#     data = pgbotdb.get_chat_data(chat_id=4)
-#     for dat in data:
-#         name = dat[2]
-#         text = dat[3]
-#         print(name,text)
-#         comentmas.append(f'{name}: {text}')
+def readData(comentmas: list):
+    data = pgbotdb.get_chat_data(chat_id=4)
+    for dat in data:
+        name = dat[2]
+        text = dat[3]
+        print(name,text)
+        comentmas.append(f'{name}: {text}')
 
 
-# run(handler_class=HttpGetHandler)
+run(handler_class=HttpGetHandler)
